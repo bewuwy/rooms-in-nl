aadd5d0987858990eaf7d2b0fdc1673654421f55
bewu <bewuwy@gmail.com>
2024-04-18 22:09:01 +0200

    added checking if config exists

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ import time
 from scrape import get_rooms
 from tg_bot import send_telegram_message
 
-
 # load config
 COUNTRY, CITY = None, None
 
-with open("config.yaml", "r") as file:
-    config = yaml.safe_load(file)
-    COUNTRY = config["XIOR_COUNTRY"]
-    CITY = config["XIOR_CITY"]
+try:
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+        COUNTRY = config["XIOR_COUNTRY"]
+        CITY = config["XIOR_CITY"]
+except FileNotFoundError:
+    # copy config_example.yaml to config.yaml
+    from shutil import copyfile
+    copyfile("config_example.yaml", "config.yaml")
+    
+    print("Please edit config.yaml and re-run the program")
+    quit()
 
 def check_rooms():
     print("Checking rooms")
@@ -30,6 +37,7 @@ def check_rooms():
 
 if __name__ == "__main__":
     
+    # check every 10 minutes
     schedule.every(10).minutes.do(check_rooms)
 
     while True:
