97b3791f8beb8637c1b92b6640851647fb23d6fa
bewu <bewuwy@gmail.com>
2024-05-05 20:46:11 +0200

    added closing date info to roomnl

--- roomnl.py
+++ roomnl.py
@@ -1,9 +1,11 @@
 import requests
+# import json
 
 from roomnl_config import ROOMNL_CONFIG, SEARCH_PRIORITY
 from tg_bot import send_telegram_message
+from datetime import datetime
 
-def get_number_of_rooms(priority_rooms: bool):
+def get_rooms(priority_rooms: bool):
     url = "https://roomapi.hexia.io/api/v1/actueel-aanbod"
     querystring = {"limit":"30","locale":"en_GB","page":"0"}
 
@@ -30,6 +32,12 @@ def get_number_of_rooms(priority_rooms: bool):
 
     response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
     r = response.json()
+    
+    # # save response to json
+    # with open("roomnl.json", "w") as f:
+    #     json.dump(r, f, indent=4)
+        
+    rooms = r["data"]
     num_rooms = r["_metadata"]["total_search_count"]
     
     if response.status_code != 200:
@@ -37,14 +45,37 @@ def get_number_of_rooms(priority_rooms: bool):
         send_telegram_message(f"Failed to get ROOM.nl rooms: {response.text}")
         quit()
 
-    return num_rooms
+    return rooms, num_rooms
 
 if __name__ == "__main__":
     
-    num_rooms = get_number_of_rooms(SEARCH_PRIORITY)
-    print(num_rooms, f"room{'s' if num_rooms > 1 else ''}{' with priority' if SEARCH_PRIORITY else ''} found")
-        
+    rooms, num_rooms = get_rooms(SEARCH_PRIORITY)
+    
     if num_rooms > 0:        
-        send_telegram_message(f"{num_rooms} ROOM.nl room{'s' if num_rooms > 1 else ''}{' with PRIORITY' if SEARCH_PRIORITY else ''} {'are' if num_rooms > 1 else 'is'} now available!\n\nSee them at https://room.nl/en/offerings/to-rent#?gesorteerd-op=zoekprofiel&voorrang=1")
+        msg = f"{num_rooms} ROOM.nl room{'s' if num_rooms > 1 else ''}{' with PRIORITY' if SEARCH_PRIORITY else ''} " + \
+            f"{'are' if num_rooms > 1 else 'is'} now available!\n\n"
+        
+        closing_dates = {}
+        for room in rooms:
+            closing_date = room["closingDate"]
+            closing_date = datetime.strptime(closing_date, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+            
+            closing_dates[closing_date] = closing_dates.get(closing_date, 0) + 1
+        
+        # sort closing dates
+        closing_dates_i = sorted(closing_dates.keys())
+        
+        for closing_date in closing_dates_i:
+            count = closing_dates[closing_date]
+            in_days = (closing_date - datetime.now().date()).days
+            in_days = f"(in {in_days} days)" if in_days > 0 else "(today!)"
+            
+            msg += f"- {count} room{'s' if count > 1 else ''} closing on {closing_date.strftime('%d.%m')} {in_days}\n"
+        
+        msg += "\nSee them at https://room.nl/en/offerings/to-rent#?gesorteerd-op=zoekprofiel&voorrang=1"
+        
     elif num_rooms == 0:
-        send_telegram_message(f"No ROOM.nl rooms{' with PRIORITY' if SEARCH_PRIORITY else ''} are available. Try again later.")
+        msg = f"No ROOM.nl rooms{' with PRIORITY' if SEARCH_PRIORITY else ''} are available. Try again later."
+
+    send_telegram_message(msg)
+    print(msg)
